c3c009b69a42992fb907bb45962f3fec8cea81c8
Ivan Dyachenko <vandyachen@gmail.com>
2015-06-12 23:44:09 +0300

    'getText' is fixed

--- cad.js
+++ cad.js
@@ -1,6 +1,6 @@
 (function() {
     function getText(element) {
-        var text = element.nnerText || element.textContent;
+        var text = element.innerText || element.textContent;
         return text;
     }
 
