1f470b07aaf78ee07e20f1653b94aba4be35e17f
Ivan Dyachenko <vandyachen@gmail.com>
2015-06-13 00:05:29 +0300

    'get theme' is added

--- cad.js
+++ cad.js
@@ -4,8 +4,14 @@
         return text;
     }
 
-    function formatText(x) {
-        return x.replace(/^\s+|\s+$/gm,'').replace(/(\r\n|\n|\r)/gm,' ').replace(/\s\s+/gm,' ');
+    function formatText(text) {
+        return text.replace(/(\r\n|\n|\r)/gm,'').replace(/\s+/g,'')
+    }
+
+    function spanToTheme(span) {
+        var theme = getText(span);
+        theme = formatText(theme);
+        return theme;
     }
 
     function tableToQuestion(table) {
@@ -14,19 +20,28 @@
         for (var i=0; i < cells.length; i++) {
             var children = cells[i].childNodes;
             for (var j=0; j < children.length; j++) {
-                question = question.concat(getText(children[j]) + " ");
+                question = question.concat(getText(children[j]));
             }
         }
         question = formatText(question);
         return question;
     }
 
+    function getTheme() {
+        var THEME_SPAN_INDEX = 0;
+        var span = document.getElementsByTagName('span');
+        var theme = spanToTheme(span[THEME_SPAN_INDEX]);
+        console.log(theme);
+    }
+
     function getQuestion() {
+        var QUESTION_TABLE_INDEX = 1;
         var tables = document.getElementsByTagName('table');
-        var question = tableToQuestion(tables[1]);
+        var question = tableToQuestion(tables[QUESTION_TABLE_INDEX]);
         console.log(question);
     }
 
+    getTheme();
     getQuestion();
 
 })();
