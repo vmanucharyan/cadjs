f135c50b144fbfeb399c9010c4f8b980de4f9029
Vardan Manucharyan <sd003gm@gmail.com>
2015-06-13 03:28:42 +0300

    categories

--- questions-parser/qparser.py
+++ questions-parser/qparser.py
@@ -9,6 +9,7 @@ def parse(source_file):
         questions = []
 
         active_prefix = ""
+        cat_index = -1
 
         while True:
             ln = f.readline()
@@ -21,7 +22,12 @@ def parse(source_file):
             if ln.endswith("{"):
                 active_prefix = ln[0:len(ln) - 1]
 
-            elif (ln.startswith("№")):
+            elif ln.startswith("##"):
+                cat_name = f.readline().strip()
+                questions.append({'name': cat_name, 'questions': []})
+                cat_index += 1
+
+            elif ln.startswith("№"):
                 question = f.readline().strip()
                 answer = f.readline() \
                     .replace("Ответ:", "") \
@@ -33,7 +39,7 @@ def parse(source_file):
                 if active_prefix != "":
                     question = active_prefix + ' ' + question
 
-                questions.append({
+                questions[cat_index]['questions'].append({
                     'question': question,
                     'answer': answer.strip()
                 })
@@ -49,9 +55,8 @@ def parse(source_file):
 
 if __name__ == "__main__":
     q = parse("questions.src")
-    print("questions count {}".format(len(q)))
 
-    obj = {'questions': q}
+    obj = {'categories': q}
 
     with open("out.json", "w") as of:
         json.dump(obj, of, ensure_ascii=False, indent=4)
