a903ed9cdd6f5f453992de0770ecc5e82daeeac0
Ivan Dyachenko <vandyachen@gmail.com>
2015-06-13 19:10:26 +0300

    'Levenshtein_distance' chars

--- cad.js
+++ cad.js
@@ -1,33 +1,31 @@
 (function() {
-    function diffChars(a, b) {
-        var n = a.length;
-        var m = b.length;
-        var MAX = n + m;
-        var v = []; //array [-MAX, MAX] of Integer
-        v[1] = 0;
-        for (var d = 0; d <= MAX; d++) {
-            for (var k = -d; k <= d; k+=2) {
-                var x;
-                if ((k == -d) || (k != d) && (v[k-1] < v[k+1])) {
-                    x = v[k+1];
-                } else {
-                    x = v[k-1] + 1;
-                }
-                var y = x - k;
-                while ((x < n) && (y < m) && (a[x+1] == b[y+1])) {
-                    x = x + 1;
-                    y = y + 1;
-                }
-                v[k] = x;
-                if ((x >= n) && (y >= m)) {
-                    console.log("Length of an SES is D=", d);
-                    return d; //TODO: flag = true;
-                }
+
+    function Levenshtein_distance (s, t) {
+        if (s.length == 0) return t.length;
+        if (t.length == 0) return s.length;
+
+        var v0 = new Array(t.length + 1);
+        var v1 = new Array(t.length + 1);
+
+        for (var i = 0; i < v0.length; i++)
+            v0[i] = i;
+
+        for (i = 0; i < s.length; i++) {
+            v1[0] = i + 1;
+
+            for (var j = 0; j < t.length; j++) {
+                var cost = (s[i] == t[j]) ? 0 : 1;
+                v1[j + 1] = Math.min(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost);
             }
+
+            for (j = 0; j < v0.length; j++)
+                v0[j] = v1[j];
         }
-        console.log("Length of an SES is greater than MAX=", MAX);
-        return MAX;
+
+        return v1[t.length];
     }
+
+
 
     function getText(element) {
         var text = element.innerText || element.textContent;
@@ -77,6 +75,9 @@
 
     var _a = "Дан цилиндрический стержень длиной L и площадью поперечного сечения S. Цилиндрическая поверхность стержня теплоизолирована. Удельная теплоемкость - cт, удельная теплопроводность - rт, плотность - p. Для решения задачи методом конечных разностей построена одномерная расчетная сетка с шагом hx=1, как это показано на рис.Решить следующую одномерную нестационарную задачу: L=4, S=1, r т=10, ст=20, p=1. Левый торец стержня имеет постоянную температуру T0=500 градусов (граничные условия первого рода), на правом торце имеет место теплообмен с внешней средой, описываемый выражением Jправ=30*(T4-T*) (граничные условия второго рода), где Jправ - плотность потока тепловой энергии, T*=100 градусов - температура внешней среды . Начальные условия: T10= T20=T30= T40=0. Используя явную вычислительную схему метода конечных разностей, определить T31 и T41 при шаге ht=1?";
     var _b = "Решить след нестац-ю задачу: L=4, S=1, rт10, ст=20, p=1. Левый T 0= 500 на правом теплообмен Jправ=30*(T4-T*) T*=100 градусов - температура внешней среды . Нач усл: T10= T20=T30= T40=0. Исп явную выч-ю схему мет конеч разн, опр T31 и T41 при шаге ht=0.5?";
-    diffChars(_a, _b);
+    //for (var i=0; i<100; i++) {
+    //    Levenshtein_distance(_a, _b)
+    //}
+    console.log(Levenshtein_distance(_a, _b));
 
 })();
